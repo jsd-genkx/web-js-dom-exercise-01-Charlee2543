04c8953f7a279f4a797a2a20340636f987f00727
Charlee2543 <Charlee2543@hotmail.com>
2024-05-20 13:47:01 +0700

    Ex_domButtomHelloWold

---
+++ Ex_dom.js
@@ -0,0 +1,7 @@
+function ClickChang() {
+    let headTop = document.querySelector("#elemenHead");
+    headTop.innerText = "Hello JavaScript InnerHTML!";
+}
+function refrech(){
+    let refrechTop = document.querySelector("#elemenHead").innerText="Hello World!"
+}
